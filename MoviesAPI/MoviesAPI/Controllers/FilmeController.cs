8930d113aa6798fa3fc8c391207221a45e8d33c8
Leandro <leandro.silva@marttech.com.br>
2022-06-06 15:46:42 -0300

    feat: criando projeto

---
+++ MoviesAPI/MoviesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class FilmeController
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void adicionarFilme([FromBody]Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filmes;
+        }
+
+        [HttpGet("{id}")]
+        public Filme RecuperaFilmePorId(int id)
+        {
+            foreach(Filme filme in filmes){
+                if(filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+            return null;
+        }
+    }
+}
